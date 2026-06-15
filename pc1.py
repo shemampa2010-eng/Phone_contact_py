8acfbff2076dec66bb348d474f0644c0a69fdb47
Paulin <shemampa2010@gmail.com>
2026-06-15 11:01:58 +0200

    Python program that works like a simple contact book. The program allows users,to save, view, search, and delete contacts.

--- pc1.py
+++ pc1.py
@@ -1,37 +1,53 @@
-def add_contact():
-    name = input ("Nom et prenom: ")
-    phone = input("Numero de tel: ")
-    email = input ("Email: ")
-    contacts.append ({
+def add_contact(name, phone, email):
+    contact = {
         "name": name,
         "phone": phone,
         "email": email
-      })
+    }
 
-    print ("New contact added: \n" , contacts)
+    return contact
 
-def remove_contact():
-    target_contact = input ("Name of the contact you want to delete: ")
-    for contact in contacts :
-        if contact["name"] == target_contact :
-            contacts.remove (contact)
-            print ("Contact", target_contact , "has been removed")    
+def remove_contact(contacts, target_contact):
+    for contact in contacts:
+        if contact["name"] == target_contact:
+            contacts.remove(contact)
+            print("Contact", target_contact, "has been removed")
 
-def review_contact():
-    print ("---Contact list---\n" , contacts)
+def review_contact(contacts):
+    print("---Contact list---\n", contacts)
 
-
+def count_contact(contacts):
+    print("Total contact", len(contacts))
 
 contacts = []
 
-while True :
-    operation = input ("Which operation do you want to do ? ""\n 1: To review a contact" "\n 2: To add a contact" "\n 3: To delete a contact " "\n operation number: " )
-    if operation == "2" :
-        add_contact()
+while True:
+    operation = input(
+        "Which operation do you want to do ?"
+        "\n 1: To review a contact"
+        "\n 2: To add a contact"
+        "\n 3: To delete a contact"
+        "\n 4: To quit"
+        "\n operation number: "
+    )
+
+    if operation == "2":
+        name = input("Nom et prenom: ")
+        phone = input("Numero de tel: ")
+        email = input("Email: ")
+
+        contacts.append(add_contact(name, phone, email))
+        print("New contact added")
+
     elif operation == "1":
-        review_contact()
+        review_contact(contacts)
+
     elif operation == "3":
-        remove_contact()   
-    else :
-        print ("--Operation invalid--\n")
-        
+        target_contact = input("Name of the contact you want to delete: ")
+        remove_contact(contacts, target_contact)
+
+    elif operation == "4":
+        break
+
+    else:
+        print("--Operation invalid--\n")
